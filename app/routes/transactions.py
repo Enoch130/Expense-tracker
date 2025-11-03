f5583c55026bc8c4a5cf8184d58cf7ce1e29f7b6
Enoch Tetteh Nartey <enochnartey130@gmail.com>
2025-11-03 16:24:04 +0000

    Another one

--- app/routes/transactions.py
+++ app/routes/transactions.py
@@ -38,3 +38,10 @@ async def get_transactions(id:UUID,db:Session=Depends(get_db)):
     db_user = fetch_user_by_id(id=id, dbase=db)
     db_transctions = db.query(Transaction).filter(Transaction.owner_id == id).all()
     return db_transctions
+
+
+@transaction_route.delete("/delete_transaction",response_model=List[TransactionOut])
+async def get_transactions(id:UUID,db:Session=Depends(get_db)):
+    db_user = fetch_user_by_id(id=id, dbase=db)
+    db_transctions = db.query(Transaction).filter(Transaction.owner_id == id).all()
+    return db_transctions
