e3f636163b0142981d970943dd1d45ebdf616b6b
Enoch Tetteh Nartey <enochnartey130@gmail.com>
2025-11-08 12:25:52 +0000

    another

---
+++ electricity.py
@@ -0,0 +1,8 @@
+citizens = int(input("Enter the number of citizens to register: "))
+count = 0
+for num in range(1,citizens + 1):
+    age = int(input(f"Enter the age of the citizen {num}: "))
+    if age >= 18:
+        count +=1
+print(count)
+5
